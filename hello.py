491f7e0e83cdf76d0e4b10d70fa6b3275ab9b700
AlwaysNasty <angrykd00@gmail.com>
2023-02-05 22:59:13 +0300

    add hello.py

--- hello.py
+++ hello.py
@@ -17,37 +17,56 @@
 #     print('menshe')
 
 
-number = 11
-running = True
+# number = 11
+# running = True
+#
+# while running:
+#     guess = int(input('Введите целое число: '))
+#     if guess == number:
+#         print('Поздравляю, вы угадали.')
+#         running = False
+#     elif guess < number:
+#         print('Нет, загаданное число немного больше этого.')
+#     else:
+#         print('Нет, загаданное число немного меньше этого.')
+#
+#
+# while True:
+#     s = input('Введите что-нибудь : ')
+#
+#     if s == 'выход':
+#         break
+#     print('Длина строки:', len(s))
+# print('Завершение')
+#
+#
+# while True:
+#     text = input('enter text: ')
+#
+#     if text == 'exit':
+#         break
+#     if len(text) < 3:
+#         print('malo blyad')
+#         continue
+#     print('norm blyad')
+#
+#
+# for i in range(50):
+#     print(i)
+#     if i == 25:
+#         print('hvatit')
+#         break
 
-while running:
-    guess = int(input('Введите целое число: '))
-    if guess == number:
-        print('Поздравляю, вы угадали.')
-        running = False
-    elif guess < number:
-        print('Нет, загаданное число немного больше этого.')
-    else:
-        print('Нет, загаданное число немного меньше этого.')
 
 
-while True:
-    s = input('Введите что-нибудь : ')
+#bubble sort
 
-    if s == 'выход':
-        break
-    print('Длина строки:', len(s))
-print('Завершение')
+arr = [2,4,1,3,6,8,5,7,0,9]
+print(arr)
 
+for i in range(len(arr)):
+    for j in range(len(arr)-i-1):
+        if arr[j] > arr[j+1]:
+            arr[j], arr[j+1] = arr[j+1] , arr[j]
 
-while True:
-    text = input('enter text: ')
-
-    if text == 'exit':
-        break
-    if len(text) < 3:
-        print('malo blyad')
-        continue
-    print('norm blyad')
-
-
+print(arr)
