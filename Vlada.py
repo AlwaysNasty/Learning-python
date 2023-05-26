5d577e15595b3c9d40ea97b21921774f6aa54e85
AlwaysNasty <angrykd00@gmail.com>
2023-05-26 18:35:13 +0300

    add Vlada.py

--- Vlada.py
+++ Vlada.py
@@ -11,6 +11,7 @@ for i in range(len(shots)-1):
     if abs(shots[i + 1] - first) > maxdif:
         maxdif = abs(shots[i+1] - first)
     first = shots[i+1]
+
 print(maxdif)
 
 minval = min(shots)
@@ -25,17 +26,20 @@ for i in range(len(shots)):
         lenmin = 0
     if lenmin > maxlen:
         maxlen = lenmin
+
 print(maxlen)
 
 first = shots[0]
+
 for i in range(len(shots) - 1):
     if shots[i + 1] == first:
         lenmin += 1
     else:
         maxlen = lenmin
         lenmin = 1
-first = shots[i+1]
+    first = shots[i+1]
 
 if lenmin > maxlen:
     axlen = lenmin
+
 print(maxlen)
